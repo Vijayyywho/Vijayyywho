94d6a8d43d89211ff0d6f0139042c11be1d77d06
Your Name <vijayyywho@gmail.com>
2022-08-28 16:45:00 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+import "bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import React from "react";
-import Home from "./Home"
-import Service  from "./Service";
+import Home from "./Home";
+import Service from "./Service";
 import Contact from "./Contact";
 import About from "./About";
 import Navbar from "./Navbar";
-import Footer from './Footer';
+import Footer from "./Footer";
 // import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import{Switch, Route,Redirect} from "react-router-dom";
-const App =() =>{
-    return(
-<>
-<Navbar />
-<switch>
-<Route exact path="/" component={Home}/>
-<Route exact path="/about" component={About}/>
-<Route exact path="/contact" component={Contact}/>
-<Route exact path="/service" component={Service}/>
-<Redirect to="/" />
-</switch>
-<Footer/>
-</>
-    );
+import { Switch, Route, Redirect } from "react-router-dom";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/service" component={Service} />
+        <Redirect to="/" />
+      </Switch>
+      <Footer />
+    </>
+  );
 };
-export default  App;
+export default App;
